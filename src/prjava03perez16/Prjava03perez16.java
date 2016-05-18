c70660028ffcc6121777cd053d880168c828eef6
ubu1 <ubu1@ubu64DVL-perez16>
2016-05-18 20:50:36 +0200

    Versio 0.1-A del projecte prjava03perez16.nomes branca00 creada y modificada

--- src/prjava03perez16/Prjava03perez16.java
+++ src/prjava03perez16/Prjava03perez16.java
@@ -35,8 +35,8 @@ public class Prjava03perez16 extends Frame
         // "this" Frame fires WindowEvent its registered WindowEvent listener 
         // "this" Frame adds "this" object as a WindowEvent listener 
 
-        setTitle("prjava03");  // "this" Frame sets title 
-        setSize(250, 100);        // "this" Frame sets initial size 
+        setTitle("nova finestra prjava03perez16");  // "this" Frame sets title 
+        setSize(450, 100);        // "this" Frame sets initial size 
         setVisible(true);         // "this" Frame shows 
     }
 
